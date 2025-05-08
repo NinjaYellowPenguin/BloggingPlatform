7b904e5ce8c00b326e9568b17e5f904b3ba844e0
Carlos Espinosa Diz <carlos.unity.developer@gmail.com>
2025-05-08 16:33:29 +0200

    feat: add postman collection

--- BloggingPlatform/src/main/java/yellowpenguin/ninja/controller/PostController.java
+++ BloggingPlatform/src/main/java/yellowpenguin/ninja/controller/PostController.java
@@ -5,8 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;	
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +14,7 @@ import yellowpenguin.ninja.dto.post.CreatePostRequest;
 import yellowpenguin.ninja.dto.post.DeletePostRequest;
 import yellowpenguin.ninja.dto.post.DeletePostResponse;
 import yellowpenguin.ninja.dto.post.PostResponse;
+import yellowpenguin.ninja.dto.post.ReadPostRequest;
 import yellowpenguin.ninja.dto.post.UpdatePostRequest;
 import yellowpenguin.ninja.services.PostService;
 
@@ -22,26 +22,26 @@ import yellowpenguin.ninja.services.PostService;
 @RequestMapping("/api/posts")
 public class PostController {
 
-	 @Autowired
-	    private PostService postService;
+	@Autowired
+	private PostService postService;
 
-	    @PostMapping
-	    public ResponseEntity<PostResponse> create(@RequestBody CreatePostRequest request) {
-	        return ResponseEntity.status(HttpStatus.CREATED).body(postService.create(request));
-	    }
+	@PostMapping
+	public ResponseEntity<PostResponse> create(@RequestBody CreatePostRequest request) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postService.create(request));
+	}
 
-	    @GetMapping("/{id}")
-	    public ResponseEntity<PostResponse> read(@PathVariable String id) {
-	        return ResponseEntity.ok(postService.read(id));
-	    }
+	@GetMapping
+	public ResponseEntity<PostResponse> read(@RequestBody ReadPostRequest request) {
+		return ResponseEntity.ok(postService.read(request.getId()));
+	}
 
-	    @PutMapping
-	    public ResponseEntity<PostResponse> update(@RequestBody UpdatePostRequest request) {
-	        return ResponseEntity.ok(postService.update(request));
-	    }
+	@PutMapping
+	public ResponseEntity<PostResponse> update(@RequestBody UpdatePostRequest request) {
+		return ResponseEntity.ok(postService.update(request));
+	}
 
-	    @DeleteMapping
-	    public ResponseEntity<DeletePostResponse> delete(@RequestBody DeletePostRequest request) {
-	        return ResponseEntity.ok(postService.delete(request));
-	    }
+	@DeleteMapping
+	public ResponseEntity<DeletePostResponse> delete(@RequestBody DeletePostRequest request) {
+		return ResponseEntity.ok(postService.delete(request));
+	}
 }
---
+++ BloggingPlatform/src/main/java/yellowpenguin/ninja/dto/post/ReadPostRequest.java
@@ -0,0 +1,12 @@
+package yellowpenguin.ninja.dto.post;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReadPostRequest {
+	
+	private String id;
+
+}
